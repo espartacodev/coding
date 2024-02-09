7d81745796464c2e85ea0d11f4bd956cc5447535
void@localhost~ <void@localhost~>
2024-02-09 06:46:32 -0300

    fix: update

--- test_my_module.py
+++ test_my_module.py
@@ -6,3 +6,12 @@ from my_module import add
 class TestAddFunction(unittest.TestCase):
     def test_add_positive_numbers(self):
         self.assertEqual(add(1, 2), 3)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(10, -5), 5)
+
+if __name__ == '__main__':
+    unittest.main()
